2ccd4c4cf6760a3170bc517fe5df3a4d19b99520
t-hunsberger <t-hunsberger@onu.edu>
2024-03-28 08:35:14 -0400

    Completed countCharacter Function

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
-int main() {
+int countCharacter(string str) {
+	int count = 0;
+	for (char c : str) {
+		count++;
+	}
+	return count;
+}
 
+int main() {
+	string testString1 = "Hello, World!";
+	string testString2 = "This is a test string.";
+
+	cout << "Number of characters in testString1; " << countCharacter(testString1) << endl;
+	cout << "Number of characters in testString2; " << countCharacter(testString2) << endl;
+
+	return 0;
 }
